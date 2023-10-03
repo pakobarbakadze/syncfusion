a4bf51ff8fa71bff20c52e09c08a40c66da0d6c8
pakobarbakadze <pakobarbakadze@gmail.com>
2023-10-03 13:11:54 +0400

    feat: cartesian

--- lib/widgets/syncfusion/cartesian_sync.dart
+++ lib/widgets/syncfusion/cartesian_sync.dart
@@ -1,96 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:test/theme.dart';
 import 'package:test/widgets/chart_card.dart';
 
-class CartesianSync extends StatefulWidget {
-  const CartesianSync({super.key});
+class CartesianSync extends StatelessWidget {
+  CartesianSync({super.key});
 
-  static List<SplineAreaSeries<SalesData, num>> getDefaultData() {
-    final List<SalesData> chartData = <SalesData>[
-      SalesData(DateTime(2005, 0, 1), 'India', 1.5, 21, 28, 680, 760),
-      SalesData(DateTime(2006, 0, 1), 'China', 2.2, 24, 44, 550, 880),
-      SalesData(DateTime(2007, 0, 1), 'USA', 3.32, 36, 48, 440, 788),
-      SalesData(DateTime(2008, 0, 1), 'Japan', 4.56, 38, 50, 350, 560),
-      SalesData(DateTime(2009, 0, 1), 'Russia', 5.87, 54, 66, 444, 566),
-      SalesData(DateTime(2010, 0, 1), 'France', 6.8, 57, 78, 780, 650),
-      SalesData(DateTime(2011, 0, 1), 'Germany', 8.5, 70, 84, 450, 800),
-      SalesData(DateTime(2012, 0, 1), 'Italy', 9.6, 78, 96, 500, 900),
-      SalesData(DateTime(2013, 0, 1), 'Sweden', 10.8, 80, 106, 600, 750),
-      SalesData(DateTime(2014, 0, 1), 'UK', 11.8, 87, 115, 700, 800),
-      SalesData(DateTime(2015, 0, 1), 'Latvia', 13.5, 95, 130, 800, 900),
-      SalesData(DateTime(2016, 0, 1), 'Denmark', 15.8, 102, 145, 900, 950),
-    ];
-    return <SplineAreaSeries<SalesData, num>>[
-      SplineAreaSeries<SalesData, num>(
-        dataSource: chartData,
-        xValueMapper: (SalesData sales, _) => sales.sales2,
-        yValueMapper: (SalesData sales, _) => sales.sales1,
-        borderWidth: 3,
-        borderDrawMode: BorderDrawMode.top,
-        borderColor: MyTheme.cyan,
-        enableTooltip: true,
-        gradient: MyTheme.cyanGradient,
-        dataLabelSettings: const DataLabelSettings(
-          labelPosition: ChartDataLabelPosition.inside,
-        ),
-      ),
-      SplineAreaSeries<SalesData, num>(
-        dataSource: chartData,
-        xValueMapper: (SalesData sales, _) => sales.sales2,
-        borderWidth: 3,
-        borderDrawMode: BorderDrawMode.top,
-        borderColor: MyTheme.blue,
-        // color: MyTheme.blue,
-        gradient: MyTheme.blueGradient,
-        yValueMapper: (SalesData sales, _) => sales.sales2,
-        dataLabelSettings: const DataLabelSettings(
-          color: Colors.white,
-        ),
-      )
-    ];
-  }
+  final List<ChartData> chartData = [
+    // Bind data source
+    ChartData(DateTime(2023, 1), 5, 3),
+    ChartData(DateTime(2023, 2), 30, 25),
+    ChartData(DateTime(2023, 3), 25, 35),
+    ChartData(DateTime(2023, 4), 45, 25),
+    ChartData(DateTime(2023, 5), 5, 50)
+  ];
 
-  @override
-  State<CartesianSync> createState() => _CartesianSyncState();
-}
-
-class _CartesianSyncState extends State<CartesianSync> {
-  String deb = '';
   @override
   Widget build(BuildContext context) {
     return ChartCard(
       child: Expanded(
         child: SfCartesianChart(
-          crosshairBehavior: CrosshairBehavior(
-              enable: true,
-              lineColor: MyTheme.purple,
-              lineWidth: 3,
-              activationMode: ActivationMode.singleTap,
-              lineType: CrosshairLineType.vertical),
-          series: CartesianSync.getDefaultData(),
-          tooltipBehavior: TooltipBehavior(enable: true, color: Colors.red),
+          plotAreaBorderWidth: 0,
+          primaryXAxis: DateTimeAxis(
+            intervalType: DateTimeIntervalType.months,
+            dateFormat: DateFormat.MMM(),
+            interval: 1,
+            axisLine: const AxisLine(
+              dashArray: <double>[1, 5],
+            ),
+            majorTickLines: const MajorTickLines(size: 0),
+            majorGridLines: const MajorGridLines(width: 0),
+          ),
+          primaryYAxis: NumericAxis(
+            axisLine: const AxisLine(
+              width: 0,
+            ),
+            majorTickLines: const MajorTickLines(size: 0),
+            majorGridLines: const MajorGridLines(
+              width: 1,
+              color: Colors.grey,
+              dashArray: <double>[1, 5],
+            ),
+          ),
+          series: <ChartSeries>[
+            _buildSplineSeries(
+              chartData: chartData,
+              borderColor: MyTheme.cyan,
+              gradientColor: MyTheme.cyanGradient,
+              yValueMapper: (ChartData data, _) => data.y,
+            ),
+            _buildSplineSeries(
+              chartData: chartData,
+              borderColor: MyTheme.blue,
+              gradientColor: MyTheme.blueGradient,
+              yValueMapper: (ChartData data, _) => data.y1,
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
-class SalesData {
-  SalesData(
-    this.numeric,
-    this.country,
-    this.sales1,
-    this.sales2,
-    this.sales3,
-    this.sales4,
-    this.sales5,
+SplineAreaSeries<ChartData, DateTime> _buildSplineSeries({
+  required List<ChartData> chartData,
+  required Color borderColor,
+  required LinearGradient gradientColor,
+  required double Function(ChartData, int) yValueMapper,
+}) {
+  return SplineAreaSeries<ChartData, DateTime>(
+    borderWidth: 3,
+    borderDrawMode: BorderDrawMode.top,
+    borderColor: borderColor,
+    enableTooltip: true,
+    gradient: gradientColor,
+    dataLabelSettings: const DataLabelSettings(
+      labelPosition: ChartDataLabelPosition.inside,
+    ),
+    dataSource: chartData,
+    xValueMapper: (ChartData data, _) => data.date,
+    yValueMapper: yValueMapper,
   );
-  final DateTime numeric;
-  final String country;
-  final double sales1;
-  final double sales2;
-  final double sales3;
-  final double sales4;
-  final double sales5;
+}
+
+class ChartData {
+  ChartData(this.date, this.y, this.y1);
+  final DateTime date;
+  final double y;
+  final double y1;
 }
